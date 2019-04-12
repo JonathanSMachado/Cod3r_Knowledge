7a70852762432a39436306ae2ea2afcf88ff683c
Jonathan S. Machado <jonathansmachado@gmail.com>
2019-04-11 22:47:18 -0300

    alterações no carregamento do database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,39 +1,13 @@
-// const connection_params = {
-//     client: '',
-//     connection: {
-//         database: '',
-//         user: '',
-//         password: ''
-//     },
-//     pool: {
-//         min: 2,
-//         max: 10
-//     },
-//     migrations: {
-//         tableName: 'knex_migrations'
-//     }
-// }
+let args = process.argv[2]
 
-const args = process.argv[2] || process.env.dbDefault
-const { [args]: db_connection} = require('../.env')
+if(!args) {
+    console.log('Changing to default database...')
+    const { dbDefault } = require('../.env')
+    args = dbDefault
+}
+
+const { [args]: db_connection } = require('../.env')
 const knex = require('knex')(db_connection)
 knex.migrate.latest([db_connection])
-
-// if(args && args === 'mysql') {   
-    // connection_params.client = 'mysql'
-    // connection_params.connection.database = 'knowledge'
-    // connection_params.connection.user = 'root'
-    // connection_params.connection.password = 'mysql'
-
-// } else {
-    // connection_params.client = 'postgresql'
-    // connection_params.connection.database = 'knowledge'
-    // connection_params.connection.user = 'postgres'
-    // connection_params.connection.password = 'postgres'
-// }
-
-// const knex = require('knex')(connection_params)
-
-// knex.migrate.latest([connection_params])
 
 module.exports = knex
