76b4a6c94743f4d99e871730daab8253314470b9
Jonathan da Silva Machado <jonathan@sadig.net>
2019-04-16 17:20:58 -0300

    Tratamentos para erros de conexão aos bancos

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,10 @@ const db = process.argv[2] || 'postgresql'
 const config = require('../.env')[db]
 const knex = require('knex')(config)
 knex.migrate.latest([config])
+    .then(_ => console.log('Conexão com o BD ativa...'))
+    .catch(err => {
+        const msg = `ERRO: Não foi possível conectar no ${ db } !`
+        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
+    })
 
 module.exports = knex
