5f98383c6cfe3b6696b56261b5066cf2104af402
Jonathan da Silva Machado <jonathan@sadig.net>
2019-04-17 16:20:59 -0300

    Ajustes no método signin

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const db = process.argv[2] || 'postgresql'
 const config = require('../.env')[db]
 const knex = require('knex')(config)
 knex.migrate.latest([config])
-    .then(_ => console.log('Conexão com o BD ativa...'))
+    .then(_ => console.log(`Conexão com o ${ db } ativa...`))
     .catch(err => {
         const msg = `ERRO: Não foi possível conectar no ${ db } !`
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
--- backend/migrations/20190411234413_add_deleted_at_table_users.js
+++ backend/migrations/20190411234413_add_deleted_at_table_users.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.alterTable('users', table => {
-        table.timestamp('deletedAt')
+        table.timestamp('deletedAt').defaultTo(null)
     })
 };
 
