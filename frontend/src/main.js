30b6a995314dc3123c8e88abb205a53b89d752b3
Jonathan S. Machado <jonathansmachado@gmail.com>
2019-05-08 00:51:51 -0300

    Finalização projeto

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,15 +3,14 @@ import Vue from 'vue'
 
 import './config/bootstrap'
 import './config/msgs'
+import './config/axios'
+import './config/mq'
 import store from './config/store'
 import router from './config/router'
 
 import App from './App'
 
 Vue.config.productionTip = false
-
-// TEMPORÁRIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkpvbmF0aGFuIE1hY2hhZG8iLCJlbWFpbCI6ImpvbmF0aGFuc21hY2hhZG9AZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU1NjkzNDMwOCwiZXhwIjoxNTU3MTkzNTA4fQ.ZcDc86Zd1WuhZgyEC9TPj-pAlPa9ZCUqCg8DpkiTheA'
 
 new Vue({
   store,
